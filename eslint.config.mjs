a9d04dd561e988b7f6f26a0a933c2a391ac6af9b
rowanna <rowankimna@gmail.com>
2025-02-13 15:56:42 +0900

    fix: build error fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
-import { defineConfig } from "eslint";
+import pkg from "eslint"; // ESLint를 default import 방식으로 가져옴
+const { defineConfig } = pkg; // 구조 분해를 통해 defineConfig 가져오기
 
 export default defineConfig({
   parserOptions: {
