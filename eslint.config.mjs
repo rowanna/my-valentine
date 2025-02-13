f1bb1d645537e5d8c23aa2d12c7b3ae8c5bea9b3
rowanna <rowankimna@gmail.com>
2025-02-13 15:51:11 +0900

    fix: build error fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  {
+    parser: "@babel/eslint-parser", // Babel parser 사용 (혹은 필요한 파서로 변경)
+
+    parserOptions: {
+      ecmaVersion: 2020, // 최신 JavaScript 문법 사용
+      sourceType: "module", // ES 모듈로 설정
+    },
+    rules: {
+      "no-eval": "error", // 동적 코드 평가 금지
+      "no-new-func": "error", // new Function 사용 금지
+    },
+  },
+];
 
 export default eslintConfig;
