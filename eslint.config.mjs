c9f53dcc1ac88b95f63498fe55a2d0c33e881518
rowanna <rowankimna@gmail.com>
2025-02-13 15:54:53 +0900

    fix: build error fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,3 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import { defineConfig } from "eslint";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-// eslint.config.mjs
 import { defineConfig } from "eslint";
 
 export default defineConfig({
